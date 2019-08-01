e1046b6e811c42f084513f4642de1d0def96d992
thinh <thinhlx1993@gmail.com>
2019-08-02 06:27:09 +0700

    test image

--- project_1_lane_finding_basic/main.py
+++ project_1_lane_finding_basic/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     resize_h, resize_w = 540, 960
 
-    verbose = True
+    verbose = False
     if verbose:
         plt.ion()
         figManager = plt.get_current_fig_manager()
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     # test on images
     test_images_dir = join('data', 'test_images')
-    test_images = [join(test_images_dir, name) for name in os.listdir(test_images_dir)]
+    test_images = [join(test_images_dir, name) for name in os.listdir(test_images_dir) if name.endswith('.jpg')]
 
     for test_img in test_images:
 
@@ -31,36 +31,36 @@ if __name__ == '__main__':
         if verbose:
             plt.imshow(out_image)
             plt.waitforbuttonpress()
-    plt.close('all')
+    # plt.close('all')
 
     # test on videos
-    test_videos_dir = join('data', 'test_videos')
-    test_videos = [join(test_videos_dir, name) for name in os.listdir(test_videos_dir)]
+    # test_videos_dir = join('data', 'test_videos')
+    # test_videos = [join(test_videos_dir, name) for name in os.listdir(test_videos_dir)]
 
-    for test_video in test_videos:
-
-        print('Processing video: {}'.format(test_video))
-
-        cap = cv2.VideoCapture(test_video)
-        out = cv2.VideoWriter(join('out', 'videos', basename(test_video)),
-                              fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
-                              fps=20.0, frameSize=(resize_w, resize_h))
-
-        frame_buffer = deque(maxlen=10)
-        while cap.isOpened():
-            ret, color_frame = cap.read()
-            if ret:
-                color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
-                color_frame = cv2.resize(color_frame, (resize_w, resize_h))
-                frame_buffer.append(color_frame)
-                blend_frame = color_frame_pipeline(frames=frame_buffer, solid_lines=True, temporal_smoothing=True)
-                out.write(cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR))
-                cv2.imshow('blend', cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR)), cv2.waitKey(1)
-            else:
-                break
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+    # for test_video in test_videos:
+    #
+    #     print('Processing video: {}'.format(test_video))
+    #
+    #     cap = cv2.VideoCapture(test_video)
+    #     out = cv2.VideoWriter(join('out', 'videos', basename(test_video)),
+    #                           fourcc=cv2.VideoWriter_fourcc(*'DIVX'),
+    #                           fps=20.0, frameSize=(resize_w, resize_h))
+    #
+    #     frame_buffer = deque(maxlen=10)
+    #     while cap.isOpened():
+    #         ret, color_frame = cap.read()
+    #         if ret:
+    #             color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
+    #             color_frame = cv2.resize(color_frame, (resize_w, resize_h))
+    #             frame_buffer.append(color_frame)
+    #             blend_frame = color_frame_pipeline(frames=frame_buffer, solid_lines=True, temporal_smoothing=True)
+    #             out.write(cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR))
+    #             cv2.imshow('blend', cv2.cvtColor(blend_frame, cv2.COLOR_RGB2BGR)), cv2.waitKey(1)
+    #         else:
+    #             break
+    #     cap.release()
+    #     out.release()
+    #     cv2.destroyAllWindows()
 
 
 
